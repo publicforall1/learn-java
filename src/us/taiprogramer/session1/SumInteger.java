1d37b4a2df7d1bc22dd044fcad04bc39a71bdf40
publicforall1 <publicforall1@gmail.com>
2020-01-21 13:46:02 +0700

    Session 1: Sum of Two Integers

---
+++ src/us/taiprogramer/session1/SumInteger.java
@@ -0,0 +1,36 @@
+/**
+ * This program use to calculate the sum of 2 integers
+ * that get from keyboard.
+ * 
+ */
+
+package us.taiprogramer.session1;
+
+/**
+ * @author taiprogramer
+ *
+ */
+public class SumInteger {
+
+	/**
+	 * @param args Unused
+	 * @return Nothing
+	 */
+	public static void main(String[] args) {
+		int a = Input.getInt("Enter #1 number: ");
+		int b = Input.getInt("Enter #2 number: ");
+		System.out.println("sum = " + sum(a, b));
+	}
+
+	/**
+	 * This method use to calculate sum of 2 integers.
+	 * 
+	 * @param a First integer
+	 * @param b Second integer
+	 * @return Sum of 2 integers
+	 */
+	public static int sum(int a, int b) {
+		return a + b;
+	}
+
+}
